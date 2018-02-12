90c74b5f89e6bd1ee6d501fa18918bb650a59d1f
zxxzking <992072389@qq.com>
2018-02-12 21:17:41 +0800

    test

--- pages/map/map.js
+++ pages/map/map.js
@@ -1,66 +1,27 @@
-// pages/map.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    markers: [{
+      iconPath: "/resources/others.png",
+      id: 0,
+      latitude: 23.099994,
+      longitude: 113.324520,
+      width: 50,
+      height: 50
+    }]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
+  onReady:function(e){
+    this.mapCtx = wx.createMapContext("map", this)
+    var mapCtx = this.mapCtx
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  regionchange(e) {
+    console.log(e.type)
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+  markertap(e) {
+    console.log(e.markerId)
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  controltap(e) {
+    console.log(e.controlId)
   }
 })
